f2ee843ac45b1de44833b0a70648f716c019f3b9
Yasser Farahi <yfarahi@risinggalaxy.com>
2022-09-08 14:11:41 +0200

    Initial commit project created

---
+++ Cat App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Cat App
+//
+//  Created by YASSER FARAHI on 08/09/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
